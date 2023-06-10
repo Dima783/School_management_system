58c7187bbb3eb8a1a7456def6010b5137acbcf73
mahmud handevi <93486297+mahmoudHidowi@users.noreply.github.com>
2023-06-10 20:49:31 +0300

    Add files via upload

---
+++ dersler.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>dersler </title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--// Meta tag Keywords -->
+    <!-- css files -->
+    <link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+    <link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+    <link rel="stylesheet" href="css/swipebox.css">
+    <link rel="stylesheet" href="css/jquery-ui.css" />
+	<link rel="stylesheet" href="css/dersler.css"/>
+    <!-- //css files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
+<body>
+<section class="portfolio-w3ls" id="gallery">
+		<h3 class="w3l-title">Bölümlerimiz</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="./images//GH1.jpg" class="swipebox"><img src="images/GH1.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Makina Öğrenme</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH2.png" class="swipebox"><img src="images/GH2.png" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Sinir Ağları</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH3.jpg" class="swipebox"><img src="images/GH3.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Görüntü işleme</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+						</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH4.png" class="swipebox"><img src="images/GH4.png" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Web</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH5.jpg" class="swipebox"><img src="images/GH5.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Front-End</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH6.png" class="swipebox"><img src="images/GH6.png" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Back-End</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+					   </div>
+				   </a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH7.jpeg" class="swipebox"><img src="images/GH7.jpeg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Database</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+					   </div>
+				   </a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH8.jpg" class="swipebox"><img src="images/GH8.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Bilgi Güvenliği</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+					   </div>
+				   </a>
+				</div>
+					<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH9.jpg" class="swipebox"><img src="images/GH9.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>oyun kodlama</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH10.jpg" class="swipebox"><img src="images/GH10.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>UX/UI</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH11.jpg" class="swipebox"><img src="images/GH11.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Bilgisayar Ağları</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 col-xs-3 gallery-grid gallery1">
+					<a href="images/GH12.jpg" class="swipebox"><img src="images/GH12.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Testing</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="clearfix"> </div>
+</section>
+<!-- //gallery -->
+     </body>
+</html>
